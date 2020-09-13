96ab7de91ebda32ff6cb240cb0adf8c6e53c22a6
Angel Simón <angelsimon@hotmail.com.ar>
2020-09-12 22:46:55 -0300

    Login Activity

--- code/app/src/main/java/com/grupo06/tp03/ui/login/LoginActivity.java
+++ code/app/src/main/java/com/grupo06/tp03/ui/login/LoginActivity.java
@@ -23,8 +23,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.grupo06.tp03.R;
-import com.grupo06.tp03.ui.login.LoginViewModel;
-import com.grupo06.tp03.ui.login.LoginViewModelFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -37,8 +35,8 @@ public class LoginActivity extends AppCompatActivity {
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
+        final EditText usernameEditText = findViewById(R.id.txtUsuario);
+        final EditText passwordEditText = findViewById(R.id.txtClave);
         final Button loginButton = findViewById(R.id.login);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
